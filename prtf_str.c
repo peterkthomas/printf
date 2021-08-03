9b9850d92103f6a26bcc9bd1d0f3549a3621745b
robert-scott <mrrobertmdscott@icloud.com>
2021-08-03 04:58:08 +0000

    Update prtf_str

--- prtf_str.c
+++ prtf_str.c
@@ -6,3 +6,30 @@
  *
  * Return: number of characters written
  */
+
+int prtf_str(va-list data)
+
+int i;
+int format = 0;
+unsigned int size;
+char str;
+int length = 0;
+int j;
+
+char str = va-arg(data, char*)
+
+for (i = 0; str[i] != '\0'; i++)
+{
+    length++;
+}
+
+size = length;
+
+for(unsigned int j = 0; j < size; j++)
+{
+    format[j] = str[i];
+
+    _putchar(format[j]);
+}
+
+return(format);
