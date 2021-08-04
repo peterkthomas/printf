4cc5f8d952a32ccef60eb1e9cafa2ca481b40492
darwindel <darwindel00@gmail.com>
2021-08-04 14:05:58 -0400

    will print char

--- prtf_chr.c
+++ prtf_chr.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "holberton.h"
+/**
+* prtf_chr - will print a character
+*
+* @data: va_list input
+*
+* Return: 0 if successful
+*/
+int prtf_chr(va_list data)
+{
+	char c;
+	int f = 0;
+
+	c = va_arg(data, int);
+
+	write(1, &c, 1);
+	return (1);
+
+}
